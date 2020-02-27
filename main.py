18d93b6cec67b2ddee6644ccfc458fa48e2024c6
Justin Miller <indymiller@gmail.com>
2020-02-27 12:13:16 -0500

    samples added

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+add_one = lambda num: num + 1
+# print(add_one(5))
+
+# add_together = lambda x, y: x + y
+# print(add_together("Hello", " World"))
+
+# odd_nums = [num for num in range(20) if num %2 != 0]
+# even_nums = [num for num in range(30) if num %2 == 0]
+# print(odd_nums)
+# print(even_nums)
+
+# sums = list(map(add_together, odd_nums, even_nums))
+# print(sums)
+
+# three_factor = list(filter(lambda x: x%3 == 0, sums))
+# print(three_factor)
